d27e3d974cb2963b005b8a1dfbcb159068e9b312
ishanPamodyaa <inishan9999@gmail.com>
2025-03-10 21:41:52 +0530

    test

--- src/main/java/edu/ICET/repocitory/OrderDetsilsRepocitory.java
+++ src/main/java/edu/ICET/repocitory/OrderDetsilsRepocitory.java
@@ -2,12 +2,13 @@ package edu.ICET.repocitory;
 
 import edu.ICET.entity.Customer;
 import edu.ICET.entity.OrderDetail;
+import edu.ICET.entity.OrderDetails_pk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface OrderDetsilsRepocitory extends JpaRepository<OrderDetail, Integer> {
+public interface OrderDetsilsRepocitory extends JpaRepository<OrderDetail, OrderDetails_pk> {
 
-
+List<OrderDetail> finfByOrder_OrderID (String orderId);
 
 }
